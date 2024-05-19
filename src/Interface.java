9b56043812b90d91dc6bc9b16c55afa2a640769f
russgibbs <russbgibbs@gmail.com>
2024-05-19 11:49:09 -0500

    commit

---
+++ src/Interface.java
@@ -0,0 +1,229 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+
+public class Interface extends JFrame implements ActionListener {
+    JPanel mainPanel;
+
+    Color background;
+
+    JTextField dragCoefficientF;
+    JTextField fluidDensityF;
+    JTextField surfaceAreaF;
+    JTextField massF;
+    JTextField velocity;
+    JTextField launchAngle;
+    JTextField gravity;
+    JTextField height;
+
+    JPanel toolBar;
+    BufferedImage graphics;
+    Graphics2D g;
+    SwingWorker<Void, Void> worker;
+    boolean cancelled;
+
+    Projectile dart;
+    double length;
+    double maxHeight;
+
+
+    JButton launch;
+    JButton setScale;
+
+    Timer timer;
+
+    public Interface() {
+        setSize(1500, 1000);
+        background = new Color(0xaacccc);
+        loadHome();
+    }
+
+    public void loadHome() {
+        setTitle("Trajectory Simulator");
+        mainPanel = new JPanel();
+        mainPanel.setBackground(background);
+        mainPanel.setVisible(true);
+        mainPanel.setLayout(new BorderLayout());
+
+        JPanel controlPanel = new JPanel();
+        controlPanel.setBackground(background);
+        controlPanel.setLayout(new GridLayout(10, 1));
+        mainPanel.add(controlPanel, BorderLayout.WEST);
+
+        JPanel[] controlPanels = new JPanel[10];
+
+        for (int i = 0; i < 10; i++) {
+            controlPanels[i] = new JPanel();
+            controlPanels[i].setLayout(new FlowLayout());
+            controlPanel.add(controlPanels[i]);
+        }
+
+        controlPanels[0].add(new JLabel("Control Panel"));
+
+        controlPanels[1].add(new JLabel("Drag Coefficient"));
+        if (dragCoefficientF == null) {
+            dragCoefficientF = new JTextField("0.38", 10);
+            dragCoefficientF.addActionListener(this);
+        }
+        controlPanels[1].add(dragCoefficientF);
+
+        controlPanels[2].add(new JLabel("Fluid Density (kgm^-3)"));
+        if (fluidDensityF == null) {
+            fluidDensityF = new JTextField("1.225", 10);
+            fluidDensityF.addActionListener(this);
+        }
+        controlPanels[2].add(fluidDensityF);
+
+        controlPanels[3].add(new JLabel("Surface Area (m^3)"));
+        if (surfaceAreaF == null) {
+            surfaceAreaF = new JTextField("0.000126", 10);
+            surfaceAreaF.addActionListener(this);
+        }
+        controlPanels[3].add(surfaceAreaF);
+
+        controlPanels[4].add(new JLabel("Mass (kg)"));
+        if (massF == null) {
+            massF = new JTextField("0.001", 10);
+            massF.addActionListener(this);
+        }
+        controlPanels[4].add(massF);
+
+
+        controlPanels[6].add(new JLabel("Velocity (ms^-1)"));
+        if (velocity == null) {
+            velocity = new JTextField("30", 10);
+            velocity.addActionListener(this);
+        }
+        controlPanels[6].add(velocity);
+
+        controlPanels[7].add(new JLabel("Launch Angle (degrees)"));
+        if (launchAngle == null) {
+            launchAngle = new JTextField("45", 10);
+            launchAngle.addActionListener(this);
+        }
+        controlPanels[7].add(launchAngle);
+
+        controlPanels[8].add(new JLabel("Gravity (ms^-2)"));
+        if (gravity == null) {
+            gravity = new JTextField("-9.81", 10);
+            gravity.addActionListener(this);
+        }
+        controlPanels[8].add(gravity);
+
+        controlPanels[9].add(new JLabel("Launch Height (m)"));
+        if (height == null) {
+            height = new JTextField("2", 10);
+            height.addActionListener(this);
+        }
+        controlPanels[9].add(height);
+
+
+        if (graphics == null) {
+            graphics = new BufferedImage(getWidth() - 400, getHeight() - 100, BufferedImage.TYPE_INT_RGB);
+            g = (Graphics2D) graphics.getGraphics();
+            g.setColor(background);
+            g.fillRect(0, 0, getWidth() - 400, getHeight() - 100);
+            g.setColor(Color.DARK_GRAY);
+            g.fillRect(100, getHeight() - 200, getWidth() - 400 - 200, 2);
+            g.fillRect(100, 100, 2, getHeight() - 100 - 200);
+        }
+        mainPanel.add(new JLabel(new ImageIcon(graphics)), BorderLayout.CENTER);
+
+
+        toolBar = new JPanel();
+        toolBar.setLayout(new FlowLayout());
+        toolBar.setVisible(true);
+        mainPanel.add(toolBar, BorderLayout.SOUTH);
+
+        if (launch == null) {
+            launch = new JButton("Launch");
+            launch.addActionListener(this);
+        }
+        toolBar.add(launch);
+
+        if (setScale == null) {
+            setScale = new JButton("Set Scale");
+            setScale.addActionListener(this);
+        }
+        toolBar.add(setScale);
+
+
+        add(mainPanel);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public void launchProjectile() {
+        dart = new Projectile(Double.parseDouble(velocity.getText()), Double.parseDouble(launchAngle.getText()),
+                Double.parseDouble(height.getText()), Double.parseDouble(dragCoefficientF.getText()),
+                Double.parseDouble(fluidDensityF.getText()), Double.parseDouble(surfaceAreaF.getText()),
+                Double.parseDouble(massF.getText()));
+        dart.launch(0.0001);
+        toolBar.add(new JLabel("Distance: " + String.format("%.2f", dart.getX_position()) + "m     "));
+        toolBar.add(new JLabel("Max Height: " + String.format("%.2f", dart.getMaxHeight()) + "m     "));
+        toolBar.add(new JLabel("Time in Flight: " + String.format("%.2f", dart.getTime()) + "s"));
+
+        dart = new Projectile(Double.parseDouble(velocity.getText()), Double.parseDouble(launchAngle.getText()),
+                Double.parseDouble(height.getText()), Double.parseDouble(dragCoefficientF.getText()),
+                Double.parseDouble(fluidDensityF.getText()), Double.parseDouble(surfaceAreaF.getText()),
+                Double.parseDouble(massF.getText()));
+
+            worker = new SwingWorker<Void, Void>() {
+            @Override
+            protected Void doInBackground() throws Exception {
+                while (!(dart.getY_position() < 0 && dart.getAngle() < 0)) {
+                    if (!cancelled) {
+                        long start = System.nanoTime();
+                        process(null);
+                        dart.move(0.0001);
+                        while (System.nanoTime() - start < 100000) {}
+                    }
+                }
+                return null;
+            }
+
+            @Override
+            protected void done() {
+                super.done();
+            }
+
+            @Override
+            protected void process(java.util.List<Void> chunks) {
+                g.fillRect((int) (100 + dart.getX_position() / length * (getWidth() - 400 - 200)),
+                        (int) (getHeight() - 200 - dart.getY_position() / length * (getWidth() - 400 - 200)),
+                        3, 3);
+                repaint();
+            }
+        };
+        cancelled = false;
+        worker.execute();
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == setScale) {
+            cancelled = true;
+            graphics = null;
+            mainPanel.setVisible(false);
+            loadHome();
+        }
+
+        if (e.getSource() == launch) {
+            cancelled = true;
+            graphics = null;
+            mainPanel.setVisible(false);
+            loadHome();
+            Projectile dart = new Projectile(Double.parseDouble(velocity.getText()), Double.parseDouble(launchAngle.getText()),
+                    Double.parseDouble(height.getText()), Double.parseDouble(dragCoefficientF.getText()),
+                    Double.parseDouble(fluidDensityF.getText()), Double.parseDouble(surfaceAreaF.getText()),
+                    Double.parseDouble(massF.getText()));
+            dart.launch(0.0001);
+            length = dart.getX_position();
+            maxHeight = dart.getMaxHeight();
+            launchProjectile();
+        }
+    }
+}
---
+++ src/Projectile.java
@@ -0,0 +1,165 @@
+public class Projectile {
+    private double velocity;
+    private double angle;
+    private double dragCoefficient;
+    private double fluidDensity;
+    private double surfaceArea;
+    private double mass;
+    private double x_position;
+    private double y_position;
+    private double time;
+    private double maxHeight;
+
+    public Projectile() {
+        velocity = 0;
+        angle = 0;
+        x_position = 0;
+        y_position = 0;
+        time = 0;
+        maxHeight = 0;
+        dragCoefficient = 0.38;
+        fluidDensity = 1.225;
+        surfaceArea = 0.000126;
+        mass = 0.001;
+    }
+
+    public Projectile(double velocity, double angle, double height, double dragCoefficient, double fluidDensity, double surfaceArea, double mass) {
+        this.velocity = velocity;
+        this.angle = angle;
+        x_position = 0;
+        y_position = height;
+        time = 0;
+        maxHeight = 0;
+        this.dragCoefficient = dragCoefficient;
+        this.fluidDensity = fluidDensity;
+        this.surfaceArea = surfaceArea;
+        this.mass = mass;
+    }
+
+    public double getVelocity() {
+        return velocity;
+    }
+
+    public double getAngle() {
+        return angle;
+    }
+
+    public double getX_position() {
+        return x_position;
+    }
+
+    public double getY_position() {
+        return y_position;
+    }
+
+    public double getMaxHeight() {
+        return maxHeight;
+    }
+
+    public double getDragCoefficient() {
+        return dragCoefficient;
+    }
+
+    public double getFluidDensity() {
+        return fluidDensity;
+    }
+
+    public double getMass() {
+        return mass;
+    }
+
+    public double getSurfaceArea() {
+        return surfaceArea;
+    }
+
+    public double getTime() {
+        return time;
+    }
+
+    public void setVelocity(double velocity) {
+        this.velocity = velocity;
+    }
+
+    public void setAngle(double angle) {
+        this.angle = angle;
+    }
+
+    public void setX_position(double x_position) {
+        this.x_position = x_position;
+
+    }
+
+    public void setY_position(double y_position) {
+        this.y_position = y_position;
+        if (y_position > maxHeight) {
+            maxHeight = y_position;
+        }
+    }
+
+    public void setDragCoefficient (double dragCoefficient) {this.dragCoefficient = dragCoefficient;}
+
+    public void setFluidDensity(double fluidDensity) {
+        this.fluidDensity = fluidDensity;
+    }
+
+    public void setMass(double mass) {
+        this.mass = mass;
+    }
+
+    public void setSurfaceArea(double surfaceArea) {
+        this.surfaceArea = surfaceArea;
+    }
+
+    public void move(double dt) {
+        setX_position(getX_position() + dt * getVelocity() * Math.cos(getAngle()*Math.PI/180) + 0.5
+                * accelerationX(getAngle(), getVelocity()) * Math.pow(dt, 2));
+        setY_position(getY_position() + dt * getVelocity() * Math.sin(getAngle()*Math.PI/180) + 0.5
+                * accelerationY(getAngle(), getVelocity()) * Math.pow(dt, 2));
+        setVelocity(getVelocity() + acceleration(getAngle(), getVelocity()) * dt);
+        setAngle(Math.atan((2 * accelerationY(getAngle(), getVelocity()) * Math.sqrt
+                ((accelerationX(getAngle(), getVelocity()) * (dt * getVelocity() * Math.cos(getAngle() *
+                        Math.PI / 180) + (accelerationX(getAngle(), getVelocity()) * Math.pow(dt, 2)) / 2)) / 2 +
+                        (Math.pow(Math.cos(getAngle() * Math.PI / 180), 2) * Math.pow(getVelocity(), 2)) / 4) +
+                (Math.sin(getAngle() * Math.PI / 180) * accelerationX(getAngle(), getVelocity()) -
+                        Math.cos(getAngle() * Math.PI / 180) * accelerationY(getAngle(), getVelocity())) *
+                        getVelocity()) / (2 * accelerationX(getAngle(), getVelocity()) *
+                Math.sqrt((accelerationX(getAngle(), getVelocity()) * (dt * getVelocity() *
+                        Math.cos(getAngle() * Math.PI / 180) + (accelerationX(getAngle(), getVelocity()) *
+                        Math.pow(dt, 2)) / 2)) / 2 + (Math.pow(Math.cos(getAngle() * Math.PI / 180), 2) *
+                        Math.pow(getVelocity(), 2)) / 4))) * 180 / Math.PI);
+    }
+
+    public void launch(double dt) {
+        do {
+            setX_position(getX_position() + dt * getVelocity() * Math.cos(getAngle()*Math.PI/180) + 0.5
+                    * accelerationX(getAngle(), getVelocity()) * Math.pow(dt, 2));
+            setY_position(getY_position() + dt * getVelocity() * Math.sin(getAngle()*Math.PI/180) + 0.5
+                    * accelerationY(getAngle(), getVelocity()) * Math.pow(dt, 2));
+            setVelocity(getVelocity() + acceleration(getAngle(), getVelocity()) * dt);
+            setAngle(Math.atan((2 * accelerationY(getAngle(), getVelocity()) * Math.sqrt
+                    ((accelerationX(getAngle(), getVelocity()) * (dt * getVelocity() * Math.cos(getAngle() *
+                            Math.PI / 180) + (accelerationX(getAngle(), getVelocity()) * Math.pow(dt, 2)) / 2)) / 2 +
+                            (Math.pow(Math.cos(getAngle() * Math.PI / 180), 2) * Math.pow(getVelocity(), 2)) / 4) +
+                    (Math.sin(getAngle() * Math.PI / 180) * accelerationX(getAngle(), getVelocity()) -
+                            Math.cos(getAngle() * Math.PI / 180) * accelerationY(getAngle(), getVelocity())) *
+                            getVelocity()) / (2 * accelerationX(getAngle(), getVelocity()) *
+                    Math.sqrt((accelerationX(getAngle(), getVelocity()) * (dt * getVelocity() *
+                            Math.cos(getAngle() * Math.PI / 180) + (accelerationX(getAngle(), getVelocity()) *
+                            Math.pow(dt, 2)) / 2)) / 2 + (Math.pow(Math.cos(getAngle() * Math.PI / 180), 2) *
+                            Math.pow(getVelocity(), 2)) / 4))) * 180 / Math.PI);
+            time += dt;
+        } while (getY_position() > 0);
+    }
+
+    public double acceleration(double angle, double velocity) {
+        return -9.81 * (Math.sin(angle*Math.PI/180)) - (dragCoefficient*surfaceArea*Math.pow(velocity, 2)*fluidDensity/2)/mass;
+    }
+
+    public double accelerationX(double angle, double velocity) {
+        return - dragCoefficient*surfaceArea*Math.pow(velocity, 2)*fluidDensity/2/mass*Math.cos(angle*Math.PI/180);
+    }
+
+    public double accelerationY(double angle, double velocity) {
+        return -9.81 - dragCoefficient*surfaceArea*Math.pow(velocity, 2)*fluidDensity/2/mass*Math.sin(angle*Math.PI/180);
+    }
+}
