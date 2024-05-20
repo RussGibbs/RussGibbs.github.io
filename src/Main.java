dc3de63b481ca781982b07b5cae956093d43fa5a
russgibbs <russbgibbs@gmail.com>
2024-05-20 18:39:35 -0500

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
-public class Main {
+import java.applet.Applet;
+
+public class Main extends Applet {
     public static void main (String[] args) {
         Interface interFace = new Interface();
     }
